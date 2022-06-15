8a9a11bac4e63d57d6858d048510a0a623b8b6a8
annameide <46717848+annameide@users.noreply.github.com>
2022-06-15 10:45:24 +0200

    template adoptation and assets

--- glacier_config.js
+++ glacier_config.js
@@ -4,17 +4,19 @@ var config = {
   showMarkers: false,
   theme: 'dark',
   use3dTerrain: true,
-  title: 'Glaciers of Glacier National Park',
-  subtitle: 'Change in coverage from 1998 to 2015',
+  // title
+  title: 'Working Title Comes Here',
+  subtitle: 'subtitle comes here',
   byline: '',
-  footer: 'Source: Story text from Wikipedia, August 2019. Data from <a href="https://www.usgs.gov/centers/norock/science/retreat-glaciers-glacier-national-park">USGS</a>',
+  footer: 'Footer text: Copyright Anna Meide, FHP, ...',
+  
   chapters: [
       {
           id: 'glacier-np',
           alignment: 'full',
-          title: 'Glacier National Park Glaciers',
+          title: 'first illustration: agricultural fields',
           image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/2015-06-19_Glacier_National_Park_%28U.S.%29_8633.jpg/800px-2015-06-19_Glacier_National_Park_%28U.S.%29_8633.jpg',
-          description: 'Glacier National Park is dominated by mountains which were carved into their present shapes by the huge glaciers of the last ice age. These glaciers have largely disappeared over the last 12,000 years. Evidence of widespread glacial action is found throughout the park in the form of U-shaped valleys, cirques, arêtes, and large outflow lakes radiating like fingers from the base of the highest peaks. Since the end of the ice ages, various warming and cooling trends have occurred. The last recent cooling trend was during the Little Ice Age, which took place approximately between 1550 and 1850. During the Little Ice Age, the glaciers in the park expanded and advanced, although to nowhere near as great an extent as they had during the Ice Age.',
+          description: 'description of the illustration if wanted comes here',
           location: {
               center: [-113.91666, 48.66451],
               zoom: 8,
