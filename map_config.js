81421125c310966cf2cc697fe77f43acec1dd6c7
annameide <46717848+annameide@users.noreply.github.com>
2022-06-27 20:41:17 +0200

    illustration alternativ tryout

--- map_config.js
+++ map_config.js
@@ -7,8 +7,8 @@ var config = {
     use3dTerrain: false,
     // title
     title: 'Working Title Comes Here',
-    subtitle: 'A science fiction short story inspired by climate / data journalism',
-    byline: 'test test test',
+    subtitle: 'A science fiction short story inspired by climate-data journalism',
+    byline: '',
     footer: 'The scrollytelling project <em>Working Title here</em> was created as part of the <em>Power of Maps. Maps of Power</em> course during the summer term 2022 at the <a href="https://design.fh-potsdam.de/University">University of Applied Sciences Potsdam</a> under the supervision of Prof. Sebastian Meier. You can find a detailed project documentation and the geodata sets on <a href="https://github.com/annameide/map-scrollytelling">GitHub</a>.<br></br>&copy; Anna Meide',
 
     chapters: [
@@ -68,7 +68,7 @@ var config = {
             title: '',
 
             // pictures are loaded from the assets folder
-            image: './assets/0_testillustration.png',
+            image: './assets/2_testillu.png',
             description: '',
             location: {
                 center: [12.9184, 52.4300],
